673b17dfd4c0441940662e441974b3d1f16536bd
noman bhimani <omegamemon@gmail.com>
2026-02-03 16:42:34 +0530

    Add base URL to Vite configuration
    Set base URL for the Vite configuration.

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base:https://github.com/nomaanbhimani/porfolio.git
     resolve: {
         alias: {
             '@': '/src',
@@ -13,3 +14,4 @@ export default defineConfig({
         target: 'esnext',
     }
 })
+
